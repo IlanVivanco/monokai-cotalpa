60210499132d06ecf1e84790becb589e2a40624e
Ilán Vivanco <ilanvivanco@gmail.com>
2021-07-31 18:52:39 +0200

    📦 NEW: Update base theme colors/settings

--- src/buildThemes.js
+++ src/buildThemes.js
@@ -1,7 +1,7 @@
 import { themeBase } from './themeBase.js'
 import { writeJSON } from './utils/file.js'
 
-const variants = ['monokai-cotalpa']
+const variants = ['monokai-cotalpa-dark']
 
 variants.map((variant) => {
 	writeJSON(`themes/theme-${variant}.json`, themeBase)
