358c1a31b7521197e48537b95e2285ed0e6b35a2
Ilán Vivanco <ilanvivanco@gmail.com>
2021-08-02 13:47:03 +0200

    📦 NEW: Add chroma util class

--- src/utils/color.js
+++ src/utils/color.js
@@ -2,8 +2,59 @@
 
 import chroma from 'chroma-js'
 
-function alpha(color, alpha = 1) {
-	return chroma(color).alpha(alpha).hex()
+class colorUtils {
+	constructor(color) {
+		this._currentValue = chroma(color)
+	}
+
+	changeColor(mode, level = 1) {
+		const currentColor = this._currentValue
+
+		switch (mode) {
+			case 'hue':
+				this._currentValue = currentColor.set('hsl.h', currentColor.get('hsl.h') + level)
+				break
+			default:
+				this._currentValue = currentColor[mode](level)
+		}
+
+		return this
+	}
+
+	alpha(level = 1) {
+		return this.changeColor('alpha', level)
+	}
+
+	darken(level = 1) {
+		return this.changeColor('darken', level)
+	}
+
+	brighten(level = 1) {
+		return this.changeColor('brighten', level)
+	}
+
+	saturate(level = 1) {
+		return this.changeColor('saturate', level)
+	}
+
+	desaturate(level = 1) {
+		return this.changeColor('desaturate', level)
+	}
+
+	hue(level = 1) {
+		return this.changeColor('hue', level)
+	}
+
+	hex() {
+		return this._currentValue.hex()
+	}
+
+	toString() {
+		return this.hex()
+	}
 }
 
-export { alpha }
+export default function (color) {
+	if (!color) return
+	return new colorUtils(color)
+}
