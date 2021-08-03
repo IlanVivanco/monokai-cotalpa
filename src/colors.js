8c3f0e83bb232d88d0b9efda6ed604811d279234
Ilán Vivanco <ilanvivanco@gmail.com>
2021-08-03 12:55:20 +0200

    📦 NEW: Add colors

--- src/colors.js
+++ src/colors.js
@@ -3,12 +3,24 @@ import gray from './palettes/cool-gray.js'
 import colors from './palettes/darker.js'
 
 export default {
+	transparent: chroma(gray.grayscale[900]).hex(),
 	strings: chroma(colors.yellow[100]).hex(),
 	attribute: chroma(colors.orange[200]).hex(),
 	operator: chroma(colors.red[300]).hex(),
 	keyword: chroma(colors.cyan[200]).hex(),
 	variable: chroma(colors.blue[300]).hex(),
 	function: chroma(colors.green[200]).hex(),
+
+	yellow: chroma(colors.yellow[100]).hex(),
+	orange: chroma(colors.orange[200]).hex(),
+	red: chroma(colors.red[300]).hex(),
+	cyan: chroma(colors.cyan[200]).hex(),
+	blue: chroma(colors.blue[300]).hex(),
+	green: chroma(colors.green[200]).hex(),
+	white: chroma(gray.grayscale[50]).hex(),
+	gray: chroma(gray.grayscale[400]).hex(),
+	darkgray: chroma(gray.grayscale[900]).hex(),
+	black: chroma(gray.grayscale[1000]).hex(),
 
 	foreground: chroma(gray.grayscale[100]).hex(),
 	focusedForeground: chroma(gray.grayscale[200]).hex(),
