2601b5c3cd8423926f99309049d827f0eb66b39a
Ilán Vivanco <ilanvivanco@gmail.com>
2021-08-05 05:02:14 +0200

    👌 IMPROVE: Theme base data

---
+++ src/rules/base.js
@@ -0,0 +1,9 @@
+import tokenColors from './simple-tokens.js'
+import colors from './workbench.js'
+
+// VSCode Theme Color reference.
+// https://code.visualstudio.com/docs/getstarted/theme-color-reference
+export default {
+	colors,
+	tokenColors,
+}
