b618f6d8644548b7049b12096a0525cce46a30d3
Ilán Vivanco <ilanvivanco@gmail.com>
2021-08-05 14:14:48 +0200

    📦 NEW: Add semantic colors

--- src/rules/base.js
+++ src/rules/base.js
@@ -1,9 +1,11 @@
-import tokenColors from './simple-tokens.js'
+import tokenColors from './tokens.js'
 import colors from './workbench.js'
+import semanticColors from './semantic.js'
 
 // VSCode Theme Color reference.
 // https://code.visualstudio.com/docs/getstarted/theme-color-reference
 export default {
 	colors,
 	tokenColors,
+	semanticColors,
 }
---
+++ src/rules/semantic.js
@@ -0,0 +1,28 @@
+export default {
+	function: {
+		foreground: ['green'],
+	},
+	method: {
+		foreground: ['green'],
+	},
+	variable: {
+		foreground: ['blue'],
+	},
+	parameter: {
+		foreground: ['orange'],
+	},
+	string: {
+		foreground: ['yellow'],
+	},
+	property: {
+		foreground: ['gray-100'],
+	},
+	interface: {
+		foreground: ['cyan'],
+		italic: true,
+	},
+	type: {
+		foreground: ['cyan'],
+		italic: true,
+	},
+}
