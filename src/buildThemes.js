20b468447b89821426c2f4e553e33f2fff55a20a
Ilán Vivanco <ilanvivanco@gmail.com>
2021-08-05 05:03:14 +0200

    👌 IMPROVE: Change build theme function

--- src/buildThemes.js
+++ src/buildThemes.js
@@ -1,9 +1,24 @@
-import { themeBase } from './themeBase.js'
+import { assignWorkbechColors, assignTokenColors } from './utils/theme.js'
 import { writeJSON } from './utils/file.js'
+import themeBase from './rules/base.js'
+import scheme from './colorScheme.js'
 
-const variants = ['monokai-cotalpa-dark']
+const themes = ['dark']
 
-variants.map((variant) => {
-	writeJSON(`themes/theme-${variant}.json`, themeBase)
-	console.log(`${variant} created!`)
+themes.forEach((variant) => {
+	console.log(`Building ${variant} theme...`)
+	const translatedWorkbechColors = assignWorkbechColors(themeBase.colors, scheme)
+	const translatedTokenColors = assignTokenColors(themeBase.tokenColors, scheme)
+
+	writeJSON(`themes/monokai-cotalpa-${variant}-color-theme.json`, {
+		name: 'Monokai Cotalpa',
+		author: 'Ilán Vivanco',
+		type: 'dark',
+		$schema: 'vscode://schemas/color-theme',
+		colorSpaceName: 'sRGB',
+
+		colors: translatedWorkbechColors,
+		tokenColors: translatedTokenColors,
+	})
+	// console.log(`${variant} created!`)
 })
