2d53b694f00d28cf6c423f35abf9ec4036dfff31
Ilán Vivanco <ilanvivanco@gmail.com>
2021-08-05 14:15:41 +0200

    👌 IMPROVE: Generate them json for package

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,7 +1,37 @@
 import fs from 'fs-extra'
 import path from 'path'
 
-export function writeJSON(filePath, data) {
+function writeJSON(filePath, data) {
 	const resolvedPath = path.resolve('.', filePath)
 	fs.outputFileSync(resolvedPath, JSON.stringify(data, null, 2))
+}
+
+async function addThemesToPackage(data = {}) {
+	const packageData = await fs.readJson('./package.json')
+	packageData.contributes.themes = data
+
+	writeJSON('./package.json', packageData)
+}
+
+export function saveThemes(themes) {
+	const metaForPackage = []
+
+	for (let theme of themes) {
+		const themeData = {
+			...theme.meta,
+			...theme.colors,
+		}
+
+		metaForPackage.push({
+			label: themeData.name,
+			uiTheme: themeData.type === 'dark' ? 'vs-dark' : 'vs',
+			path: `./${theme.path}`,
+		})
+
+		console.log(`Building ${themeData.name} theme...`)
+		writeJSON(theme.path, themeData)
+	}
+
+	// Save data to package.json
+	addThemesToPackage(metaForPackage)
 }
